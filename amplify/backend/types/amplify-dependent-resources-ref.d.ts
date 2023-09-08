27f7588fa5602f7e335af77fd9e634331fd48864
Sa <sa@Sas-MacBook-Pro.attlocal.net>
2023-09-08 15:20:00 -0500

    Diff changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -26,15 +26,6 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolName": "string"
     }
   },
-  "function": {
-    "stripe": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
   "storage": {
     "imagestorage": {
       "BucketName": "string",
